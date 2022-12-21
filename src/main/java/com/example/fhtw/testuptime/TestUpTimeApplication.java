8116d896cd9704841594096a01e9cc7d2bfb47c2
ic21b131 <ic21b131@technikum-wien.at>
2022-12-21 15:50:00 +0100

    Initial commit

---
+++ src/main/java/com/example/fhtw/testuptime/TestUpTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fhtw.testuptime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUpTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestUpTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fhtw/testuptime/TestUpTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fhtw.testuptime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUpTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
